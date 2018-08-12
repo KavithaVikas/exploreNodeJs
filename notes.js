046a109e884c77ef200b725a78ed78a83de423fd
xx18 <xx18@global.tesco.org>
2018-08-12 16:22:11 +0530

    add note using fs modules writsync

--- notes.js
+++ notes.js
@@ -1,11 +1,34 @@
 console.log("starting notes.js");
 
+const fs = require("fs");
 
-addNote = (noteName, noteContent) => {
-    console.log(`adding note name ${noteName} and note content ${noteContent}`);
+
+var addNote = (noteName, noteContent) => {
+    var notes = [];
+    let note = {
+        title: noteName,
+        body: noteContent
+    }
+    try {
+
+        let existingNotes = fs.readFileSync("notes1.json");
+        notes = JSON.parse(existingNotes);
+    } catch (e) {
+
+    }
+
+    let duplicateCheck = notes.filter(element => element.title === note.title);
+    console.log(`duplicate check ${duplicateCheck.length}`);
+
+    if (duplicateCheck.length === 0) {
+        console.log(typeof notes);
+        notes.push(note);
+        fs.writeFileSync("notes1.json", JSON.stringify(notes));
+        console.log(`adding note name ${noteName} and note content ${noteContent}`);
+    }
 }
 
-getAllNotes = () => { 
+getAllNotes = () => {
     console.log("Lists all note");
 }
 
