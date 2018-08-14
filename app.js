5d52d8a173f8ba248af2492d528f2deaa238f3d5
xx18 <xx18@global.tesco.org>
2018-08-14 13:32:33 +0530

    streaming videos and audio

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
+const mediaserver = require('mediaserver');
+const path = require('path');
+const fs  = require('fs');
 
 app.get("/", (req, res)=>{
     res.send("hello world!");
 })
 
-app.get("/api/notes", (req,res)=>{
-    res.send({"title":"Secret Book", "bookContent":"Secret for success"});
+app.get("/api/notes/:videoname", (req,res)=>{
+    var videoPath = path.join(__dirname, "videos", req.params.videoname);
+    console.log(videoPath);
+    mediaserver.pipe(req, res, videoPath);
+    //res.send({"title":"Secret Book", "bookContent":"Secret for success"});
 })
 
-app.listen(3001,()=>{
-    console.log("Server listening on port 3001");
+app.get("/api/video/:videoname", (req,res)=>{
+    res.writeHead(200, {"content-type":"video/mp4"})
+    var videoPath = path.join(__dirname, "videos", req.params.videoname);
+    var rs = fs.createReadStream(videoPath);
+    rs.pipe(res);
+})
+
+app.listen(3005,()=>{
+    console.log("Server listening on port 3005");
 })
